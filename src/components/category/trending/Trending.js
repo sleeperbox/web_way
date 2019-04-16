5628f105b4f485da908b08f55a7726cbd863117a
juheri <juheri842@gmail.com>
2019-04-16 16:35:30 +0700

    computer and gadget

--- src/components/category/trending/Trending.js
+++ src/components/category/trending/Trending.js
@@ -68,7 +68,7 @@ export default class Trending extends Component {
                     ) : (
                       <img
                         src={
-                          "http://localhost:3000/src/web-api/public/posting/foto/" +
+                          "http://192.168.100.18/src/web-api/public/posting/foto/" +
                           data.fotocontent
                         }
                       />
@@ -111,10 +111,10 @@ export default class Trending extends Component {
                    <GridList cols={5} cellHeight={100} spacing={2}>
                     {users.map(user => (
                       
-                    <Card style={{margin: 20}}>
+                    <Card style={{margin: 20}} key={user._id}>
                         <CardContent>
                             <center>
-                                <Avatar src={"http://localhost:3000/src/web-api/public/avatar/" + user.foto} height={20} width={20}/>
+                                <Avatar src={"http://192.168.100.18/src/web-api/public/avatar/" + user.foto} height={20} width={20}/>
                                 <br />
                                 <small>{"@" + user.username}</small>
                             </center>
